69cad38af818d276a45220b65c683a445e22fd43
ericmartinez01 <48879548+ericmartinez01@users.noreply.github.com>
2021-03-26 16:10:41 -0400

    completed adding workers to array
    I need to figure out how make edits to the #members-team-name and it show up in the other browser and update the workers array

--- timesheet-app/listeners.js
+++ timesheet-app/listeners.js
@@ -2,10 +2,9 @@ let summaryEl = document.createElement('span')
 
 // Listener to add a person #create-worker
 document.querySelector('#create-worker').addEventListener('click', (e) => {
-    addTeamMember('Pete')
     let p = document.createElement('p')
     p.textContent = 'new person'
     document.querySelector('#worker-summary').appendChild(p)
-    // location.assign('./timesheet.html')
-    updateWorkers(workers)
+    location.assign('./timesheet.html')
+    addTeamMember('New Person')
 })
--- timesheet-app/timesheet-page.js
+++ timesheet-app/timesheet-page.js
@@ -1,3 +1,9 @@
 document.querySelector('#home-page').addEventListener('click', e => {
     location.assign('./index.html')
 })
+
+// listener for team member name
+document.querySelector('#team-members-name').addEventListener('input', (e) => {
+    console.log(e)
+    console.log(e.target.value)
+})
